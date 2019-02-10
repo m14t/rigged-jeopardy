88c3230eb07ae36c968a24327599d3c47faa0805
Matt Farmer <matt@mattfarmer.net>
2019-02-09 21:22:00 -0800

    Updates

---
+++ precache-manifest.7518186c7d15320874ac9b289508c786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "833f16adec4fb1dbdfee",
+    "url": "/rigged-jeopardy/static/js/runtime~main.833f16ad.js"
+  },
+  {
+    "revision": "ad632fe58f856110c35c",
+    "url": "/rigged-jeopardy/static/js/main.ad632fe5.chunk.js"
+  },
+  {
+    "revision": "aa5b4f5febe41f2bd9db",
+    "url": "/rigged-jeopardy/static/js/1.aa5b4f5f.chunk.js"
+  },
+  {
+    "revision": "ad632fe58f856110c35c",
+    "url": "/rigged-jeopardy/static/css/main.265ba094.chunk.css"
+  },
+  {
+    "revision": "9bd40f2a5b120a433517d2798f7c9b91",
+    "url": "/rigged-jeopardy/index.html"
+  }
+];
