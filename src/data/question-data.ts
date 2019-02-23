ef180a47841c10e751376e7ffd62338a9d95bb79
Matt Farmer <work@mattfarmer.net>
2019-02-23 11:22:41 -0800

    Question edit round 2

--- src/data/question-data.ts
+++ src/data/question-data.ts
@@ -7,35 +7,21 @@ const questionData: QuestionData = [
       // Question 1
       {
         easy: {
-          text: 'Eliz was trapped in a silent movie this year.',
-          image: `${process.env.PUBLIC_URL}/images/Year_Photos_250_easy.png`,
-          answer: '2013',
+          text: 'When this happened.',
+          image: `${process.env.PUBLIC_URL}/images/today.jpg`,
+          answer: '????',
         },
         hard: {
-          text:
-            'In 2013, Eliz was trapped in a silent movie in a take reminiscent of silent film stars. In this year, the first narrative silent film, The Great Train Robbery, was released.',
-          image: `${process.env.PUBLIC_URL}/images/Year_Photos_250_easy.png`,
-          answer: '1903',
+          text: 'When this happened.',
+          image: `${process.env.PUBLIC_URL}/images/today.jpg`,
+          answer: '????',
         },
       },
       // Question 2
       {
         easy: {
-          text: 'The year this image was photoshopped.',
-          image: `${process.env.PUBLIC_URL}/images/Year_Photos_500_2006.jpg`,
-          answer: '2019',
-        },
-        hard: {
-          text: 'This is when Jess first learned to crochet.',
-          image: `${process.env.PUBLIC_URL}/images/Year_Photos_500_2006.jpg`,
-          answer: '1990',
-        },
-      },
-      // Question 3
-      {
-        easy: {
           text:
-            "This year's ball pit had 2011 balls and 2011 oz. of semen forever wrecked Wolf's plumbing system.",
+            "This year's ball pit had 2011 balls and 2011 tree roots forever wrecked Wolf's plumbing system.",
           image: `${process.env.PUBLIC_URL}/images/Year_Photos_750_easy.jpg`,
           answer: 'What is 2011?',
         },
@@ -45,17 +31,16 @@ const questionData: QuestionData = [
           answer: 'What is 476?',
         },
       },
-      // Question 4
+      // Question 3
       {
         easy: {
-          text: 'When this happened.',
-          image: `${process.env.PUBLIC_URL}/images/today.jpg`,
-          answer: '????',
+          text: 'Eliz was trapped in a silent movie this year.',
+          image: `${process.env.PUBLIC_URL}/images/Year_Photos_250_easy.png`,
+          answer: '2013',
         },
         hard: {
-          text: 'When this happened.',
-          image: `${process.env.PUBLIC_URL}/images/today.jpg`,
-          answer: '????',
+          video: `${process.env.PUBLIC_URL}/videos/hard.mp4`,
+          answer: '???',
         },
       },
     ],
@@ -67,29 +52,9 @@ const questionData: QuestionData = [
       {
         easy: {
           text:
-            "The year of Brexit and Trump's election couldn't stop this man from Mosh Kosh B'Goshin' his way through Forman.",
-          image: `${
-            process.env.PUBLIC_URL
-          }/images/twomb-mautic_easy_250_2016.jpg`,
-          answer: '2016',
-        },
-        hard: {
-          text: "Osh Kosh B'Gosh, est. XXXX.",
-          image: `${
-            process.env.PUBLIC_URL
-          }/images/twomb-matic_250_hard_1895.jpg`,
-          answer: '1895',
-        },
-      },
-      // Question 2
-      {
-        easy: {
-          text:
-            "Add 2002 to these sweet 16 cheeks to discover the year of Gosselin's sweet 16.",
-          image: `${
-            process.env.PUBLIC_URL
-          }/images/twomb-mautic_500_easy_2018.jpg`,
-          answer: '2018',
+            "The midnight ride of Twombs Revere comes from Gosselin's 2nd year.",
+          image: `${process.env.PUBLIC_URL}/images/twomb-mautic_750_2005.jpg`,
+          answer: '2005',
         },
         hard: {
           text:
@@ -98,21 +63,18 @@ const questionData: QuestionData = [
           answer: '2007',
         },
       },
-      // Question 3
+      // Question 2
       {
         easy: {
-          text:
-            "The midnight ride of Twombs Revere comes from Gosselin's 2nd year.",
-          image: `${process.env.PUBLIC_URL}/images/twomb-mautic_750_2005.jpg`,
-          answer: '2005',
+          image: `${process.env.PUBLIC_URL}images/Erock_Paper_Scissors.png`,
+          answer: '???',
         },
         hard: {
-          text: '????',
-          image: `${process.env.PUBLIC_URL}/images/twomb-mautic_750_2005.jpg`,
+          image: `${process.env.PUBLIC_URL}images/Erock_Paper_Scissors.png`,
           answer: '???',
         },
       },
-      // Question 4
+      // Question 3
       {
         easy: {
           text: "You'd have to be insane to not remember this 2013 posse!",
@@ -142,25 +104,12 @@ const questionData: QuestionData = [
           answer: '2004, 2005, 2007-2012, 2015, 2016, 2018',
         },
         hard: {
-          text: "This was the year when Noah Wolf BBQ'ed the most meat.",
-          // image: `${process.env.PUBLIC_URL}/images/Year_Photos_250_easy.png`,
-          answer: '2010',
-        },
-      },
-      // Question 2
-      {
-        easy: {
-          text: "Whoa, did anyone audit last year's goblin cupcakes?",
-          image: `${process.env.PUBLIC_URL}/images/food_500_easy_2018.jpg`,
-          answer: '2018',
-        },
-        hard: {
           text: "Name the 6th ingredient from 2007's Jello brand Pudding.",
           image: `${process.env.PUBLIC_URL}/images/food_500_easy_2007.jpg`,
           answer: 'Tetrasodium Pyrohosphate',
         },
       },
-      // Question 3
+      // Question 2
       {
         easy: {
           text:
@@ -175,7 +124,7 @@ const questionData: QuestionData = [
           answer: '15 fl. oz.',
         },
       },
-      // Question 4
+      // Question 3
       {
         easy: {
           text: 'Y2K + 5 = Martini stew, no doi.',
@@ -212,10 +161,8 @@ const questionData: QuestionData = [
       // Question 2
       {
         easy: {
-          text:
-            "Dave, dressed as Ellen Degeneres, coudln't get Ellen out of his mind, although the 1994-1998 smash sitcom had been cancelled 11 year prior to this Gosselin.",
-          image: `${process.env.PUBLIC_URL}/images/Celebrity_easy_500_2009.png`,
-          answer: '2009',
+          video: `${process.env.PUBLIC_URL}/videos/easy.mp4`,
+          answer: '???',
         },
         hard: {
           text: `In 2009, "Dave, dressed as Ellen Degeneres, coudln't get Ellen out of his mind."  In THIS year, Ellen Degeneres finally got cigarettes out of her mind, when she quit smoking.`,
@@ -226,29 +173,15 @@ const questionData: QuestionData = [
       // Question 3
       {
         easy: {
-          // text: '???',
-          // image: `${process.env.PUBLIC_URL}/images/food_750_2012.jpg`,
-          video: `${process.env.PUBLIC_URL}/videos/easy.mp4`,
-          answer: '???',
-        },
-        hard: {
-          // text: '???',
-          video: `${process.env.PUBLIC_URL}/videos/hard.mp4`,
-          answer: '???',
-        },
-      },
-      // Question 4
-      {
-        easy: {
           text:
             "This was the year Martini had a John Wilkes Booth to commemorate the 150th anniversary of Lincoln's 1865 assassination.",
           image: `${process.env.PUBLIC_URL}/images/celebrity_1000__2015.jpg`,
           answer: '2015',
         },
         hard: {
-          text: 'The year that John Wilkes Booth made his theatrical debut.',
-          image: `${process.env.PUBLIC_URL}/images/celebrity_1000__2015.jpg`,
-          answer: '1856',
+          // text: '???',
+          video: `${process.env.PUBLIC_URL}/videos/austin_hard.mp4`,
+          answer: '???',
         },
       },
     ],
